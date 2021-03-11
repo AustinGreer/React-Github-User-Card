98e4bd4c5b75eca312c6ac39ebd6c51f6525aade
Austin Greer <agreer.ag12@gmail.com>
2021-03-10 23:43:19 -0500

    imported axios into project. Successfully retrieved data for user in User component

--- github-usercard/src/components/User.js
+++ github-usercard/src/components/User.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import axios from 'axios'
 
 class User extends React.Component {
+    state = {
+        user: null,
+    }
+
+    componentDidMount() {
+        axios.get('https://api.github.com/users/AustinGreer')
+        .then (res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     render() {
         return (
